749488bbf4b3114e4e55e1d9ef658701ba238976
Jacob Gold <jacob@graphite.dev>
2023-10-17 11:49:57 -0400

    fix: typo

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -19,7 +19,7 @@ if not vim.g.vscode then
 		},
 	}
 
-	local builtin = require('teleucope.builtin')
+	local builtin = require('telescope.builtin')
 	vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 	vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 	vim.keymap.set('n', '<leader>fs', function()
