c2316ccf0a1e66a19363fa1686fc26e024d9a911
Jacob Gold <jacob@graphite.dev>
2023-10-16 21:59:54 -0400

    chore: format lazy.lua

--- lua/jacobgold/lazy.lua
+++ lua/jacobgold/lazy.lua
@@ -1,100 +1,102 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { 
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        'sainnhe/gruvbox-material',
-        cond = not vim.g.vscode
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        cond = not vim.g.vscode
-    },
-    {
-        'nvim-treesitter/playground',
-        cond = not vim.g.vscode
-    },
-    {
-        'ThePrimeagen/harpoon',
-        cond = not vim.g.vscode
-    },
-    {
-        'ThePrimeagen/vim-be-good',
-        cond = not vim.g.vscode
-    },
-    {
-        'mbbill/undotree',
-        cond = not vim.g.vscode
-    },
-    {
-        'tpope/vim-fugitive',
-        cond = not vim.g.vscode
-    },
-    {
-        'tpope/vim-rhubarb',
-        cond = not vim.g.vscode
-    },
-    'tpope/vim-surround',
-    'andrewradev/undoquit.vim',
-    {
-        -- Adds git related signs to the gutter, as well as utilities for managing changes
-        'lewis6991/gitsigns.nvim',
-        opts = {
-            -- See `:help gitsigns.txt`
-            signs = {
-                add = { text = '+' },
-                change = { text = '~' },
-                delete = { text = '_' },
-                topdelete = { text = '‾' },
-                changedelete = { text = '~' },
-            },
-            on_attach = function(bufnr)
-                vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-                vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-                vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
-            end,
-        },
-    },
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.2',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		'sainnhe/gruvbox-material',
+		cond = not vim.g.vscode
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		cond = not vim.g.vscode
+	},
+	{
+		'nvim-treesitter/playground',
+		cond = not vim.g.vscode
+	},
+	{
+		'ThePrimeagen/harpoon',
+		cond = not vim.g.vscode
+	},
+	{
+		'ThePrimeagen/vim-be-good',
+		cond = not vim.g.vscode
+	},
+	{
+		'mbbill/undotree',
+		cond = not vim.g.vscode
+	},
+	{
+		'tpope/vim-fugitive',
+		cond = not vim.g.vscode
+	},
+	{
+		'tpope/vim-rhubarb',
+		cond = not vim.g.vscode
+	},
+	'tpope/vim-surround',
+	'andrewradev/undoquit.vim',
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+			on_attach = function(bufnr)
+				vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+					{ buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+				vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
+					{ buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+				vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
+					{ buffer = bufnr, desc = '[P]review [H]unk' })
+			end,
+		},
+	},
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		dependencies = {
+			-- LSP Support
+			{ 'neovim/nvim-lspconfig' },           -- Required
+			{ 'williamboman/mason.nvim' },         -- Optional
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-        }
-    },
-    {
-        'zbirenbaum/copilot.lua',
-        cond = not vim.g.vscode
-    },
-    {
-        "zbirenbaum/copilot-cmp",
-        config = function ()
-            require("copilot_cmp").setup()
-        end
-    }
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' },   -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'L3MON4D3/LuaSnip' },   -- Required
+		}
+	},
+	{
+		'zbirenbaum/copilot.lua',
+		cond = not vim.g.vscode
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		config = function()
+			require("copilot_cmp").setup()
+		end
+	}
 })
-
