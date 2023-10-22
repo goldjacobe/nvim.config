49f7224b9e36181ae281cb4f95d6974f84883163
Jacob Gold <jacob@graphite.dev>
2023-10-22 16:18:20 -0400

    feat: more fugitive maps

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,22 @@
+-- open or focus fugitive
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
+
+-- close fugitive
+vim.keymap.set('n', '<leader>gq', function()
+	vim.cmd('Git')
+	vim.api.nvim_input('gq')
+end, { silent = true })
+
+-- (re-)open fugitive in a vertical split
+vim.keymap.set('n', '<leader>gv', function()
+	vim.cmd('Git')
+	vim.api.nvim_input('gq')
+	vim.cmd("vertical Git")
+end, { silent = true })
+
+-- (re-)open fugitive in a horizontal split
+vim.keymap.set('n', '<leader>gh', function()
+	vim.cmd('Git')
+	vim.api.nvim_input('gq')
+	vim.cmd("Git")
+end, { silent = true })
