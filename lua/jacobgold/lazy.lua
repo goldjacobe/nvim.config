faf5481e0aa0e5f6d370718be70f89f5b86f0fd7
Jacob Gold <jacob@graphite.dev>
2023-10-22 16:52:25 -0400

    feat: use suggested gitsigns maps for hunks

--- lua/jacobgold/lazy.lua
+++ lua/jacobgold/lazy.lua
@@ -87,12 +87,44 @@ require("lazy").setup({
 				changedelete = { text = '~' },
 			},
 			on_attach = function(bufnr)
-				vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-					{ buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-				vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
-					{ buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-				vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
-					{ buffer = bufnr, desc = '[P]review [H]unk' })
+				local gs = package.loaded.gitsigns
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map('n', ']c', function()
+					if vim.wo.diff then return ']c' end
+					vim.schedule(function() gs.next_hunk() end)
+					return '<Ignore>'
+				end, { expr = true })
+
+				map('n', '[c', function()
+					if vim.wo.diff then return '[c' end
+					vim.schedule(function() gs.prev_hunk() end)
+					return '<Ignore>'
+				end, { expr = true })
+
+				-- HunkActions
+				map('n', '<leader>hs', gs.stage_hunk)
+				map('n', '<leader>hr', gs.reset_hunk)
+				map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+				map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+				map('n', '<leader>hS', gs.stage_buffer)
+				map('n', '<leader>hu', gs.undo_stage_hunk)
+				map('n', '<leader>hR', gs.reset_buffer)
+				map('n', '<leader>hp', gs.preview_hunk)
+				map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+				map('n', '<leader>tb', gs.toggle_current_line_blame)
+				map('n', '<leader>hd', gs.diffthis)
+				map('n', '<leader>hD', function() gs.diffthis('~') end)
+				map('n', '<leader>td', gs.toggle_deleted)
+
+				-- Text object
+				map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
 			end,
 		},
 	},
