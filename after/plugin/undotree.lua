2e87db1cfcef13ef9e4f516a31310baaf7a2dcc9
Jacob Gold <jacob@graphite.dev>
2023-10-22 16:36:03 -0400

    feat: focus undotree bind

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,3 +1,7 @@
 if not vim.g.vscode then
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+	vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+	vim.keymap.set('n', '<leader>uf', function()
+		vim.cmd('UndotreeShow')
+		vim.cmd('UndotreeFocus')
+	end)
 end
