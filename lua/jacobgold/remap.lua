def3a5d684230e625c8660a6b3fefba1f5b016c0
Jacob Gold <jacob@graphite.dev>
2023-10-27 16:19:16 -0400

    feat: adjust chmod x maps

--- lua/jacobgold/remap.lua
+++ lua/jacobgold/remap.lua
@@ -62,7 +62,8 @@ vim.keymap.set("n", "<leader>cc", "<cmd>cclose<CR>")
 -- vim.keymap.set("n", "<leader>lk", "<cmd>lnext<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("n", "<leader>+x", "<cmd>!chmod +x %<CR>" )
+vim.keymap.set("n", "<leader>-x", "<cmd>!chmod -x %<CR>" )
 
 -- run yarn prettier on save yay
 vim.keymap.set("n", "<leader>w", function()
