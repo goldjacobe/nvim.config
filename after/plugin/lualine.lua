e278fc5b5e43f195867381685004aa42f4c5c06e
Jacob Gold <jacob@graphite.dev>
2023-10-30 23:35:03 -0400

    feat: noice lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -15,9 +15,24 @@ if not vim.g.vscode then
 			lualine_a = { 'buffers' },
 			lualine_b = {},
 			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = {}
+			lualine_x = {
+				{
+					require("noice").api.status.search.get,
+					cond = require("noice").api.status.search.has,
+				},
+			},
+			lualine_y = {
+				{
+					require("noice").api.status.ruler.get,
+					cond = require("noice").api.status.ruler.has,
+				},
+			},
+			lualine_z = {
+				{
+					require("noice").api.status.command.get,
+					cond = require("noice").api.status.command.has,
+				},
+			}
 		}
 	}
 end
