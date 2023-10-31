253b1817e17d7743555bb05d34eda794d4fb3583
Jacob Gold <jacob@graphite.dev>
2023-10-31 18:24:42 -0400

    feat: more noice and lualine configuration

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -7,30 +7,56 @@ if not vim.g.vscode then
 			'trouble'
 		},
 		sections = {
-			lualine_c = {
-				require('auto-session.lib').current_session_name
-			},
+			lualine_a = { 'branch' },
+			lualine_b = { 'diff' },
+			lualine_c = { 'diagnostics' },
+			lualine_x = { 'encoding' },
+			lualine_y = { 'fileformat' },
+			lualine_z = { 'filetype' },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = {},
+		},
+		winbar = {
+			lualine_a = { 'mode' },
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = { 'progress' },
+			lualine_z = { 'location' }
+		},
+		inactive_winbar = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { 'filename' },
+			lualine_x = { 'progress' },
+			lualine_y = { 'location' },
+			lualine_z = {}
 		},
 		tabline = {
-			lualine_a = { 'buffers' },
+			lualine_a = {'buffers'},
 			lualine_b = {},
 			lualine_c = {},
 			lualine_x = {
+			},
+			lualine_y = {
+				{
+					require("noice").api.statusline.mode.get,
+					cond = require("noice").api.statusline.mode.has,
+				},
 				{
 					require("noice").api.status.search.get,
 					cond = require("noice").api.status.search.has,
 				},
 			},
-			lualine_y = {
-				{
-					require("noice").api.status.ruler.get,
-					cond = require("noice").api.status.ruler.has,
-				},
-			},
 			lualine_z = {
 				{
-					require("noice").api.status.command.get,
-					cond = require("noice").api.status.command.has,
+					require('auto-session.lib').current_session_name
 				},
 			}
 		}
