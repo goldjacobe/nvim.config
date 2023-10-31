4a2b55755e68f1f338879f7a3ec3b2422a9944d3
Jacob Gold <jacob@graphite.dev>
2023-10-31 18:56:48 -0400

    feat: improve tele maps

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,7 +6,7 @@ if not vim.g.vscode then
 		},
 		extensions = {
 			file_browser = {
---				theme = "ivy",
+				--				theme = "ivy",
 				-- disables netrw and use telescope-file-browser in its place
 				hijack_netrw = true,
 				mappings = {
@@ -25,33 +25,26 @@ if not vim.g.vscode then
 	require("telescope").load_extension('harpoon')
 
 	local builtin = require('telescope.builtin')
-	vim.keymap.set({ 'n', 'v' }, '<leader>ff', builtin.find_files, {})
-	vim.keymap.set({ 'n', 'v' }, '<leader>fg', builtin.git_files, {})
-	vim.keymap.set({ 'n', 'v' }, '<leader>fo', builtin.buffers, {})
-	vim.keymap.set({ 'n', 'v' }, '<leader>fl', builtin.live_grep, {})
-	vim.keymap.set({ 'n', 'v' }, '<leader>fs', builtin.grep_string, {})
+	vim.keymap.set({ 'n', 'v' }, '<leader>ff', builtin.find_files, { desc = 'Files' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fg', builtin.git_files, { desc = 'Git files' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fo', builtin.buffers, { desc = 'Buffers' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fl', builtin.live_grep, { desc = 'Live grep' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fs', builtin.grep_string, { desc = 'Grep string' })
 	vim.keymap.set({ 'n', 'v' }, '<leader>fh', function()
 		vim.cmd('Telescope harpoon marks')
-	end)
+	end, { desc = '' })
 
-	vim.keymap.set({ 'n', 'v' }, '<leader>fq', builtin.quickfix, {})
-	vim.keymap.set({ 'n', 'v' }, '<leader>ftb', builtin.builtin, {})
-	vim.keymap.set({ 'n', 'v' }, '<leader>fgs', builtin.git_status, {})
-	vim.keymap.set({ 'n', 'v' }, '<leader>fcb', builtin.current_buffer_fuzzy_find, {})
+	vim.keymap.set({ 'n', 'v' }, '<leader>fq', builtin.quickfix, { desc = 'Quickfix' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>ft', builtin.builtin, { desc = 'Telescopes' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fG', builtin.git_status, { desc = 'Git status' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'Current buffer' })
 	require("telescope").load_extension "file_browser"
-
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>fb",
-		":Telescope file_browser<CR>",
-		{ noremap = true }
-	)
 
 	-- open file_browser with the path of the current buffer
 	vim.api.nvim_set_keymap(
 		"n",
 		"<leader>fb",
 		":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-		{ noremap = true }
+		{ noremap = true, desc = "File browser" }
 	)
 end
