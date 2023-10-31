f463f7ccfceb26514ca731443a5bcd5baa7bc9ed
Jacob Gold <jacob@graphite.dev>
2023-10-30 22:48:09 -0400

    fix: some lsp maps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -31,7 +31,6 @@ if not vim.g.vscode then
         local opts = {buffer = bufnr, remap = false}
 
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "H", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -45,9 +44,7 @@ if not vim.g.vscode then
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     end)
 
 	require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
