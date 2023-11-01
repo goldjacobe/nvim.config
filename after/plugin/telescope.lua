e55ed8aade8bb7257d8557278ef4e18763c1a327
Jacob Gold <jacob@graphite.dev>
2023-11-01 12:17:00 -0700

    feat: add more search telescopes

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -27,17 +27,33 @@ if not vim.g.vscode then
 	local builtin = require('telescope.builtin')
 	vim.keymap.set({ 'n', 'v' }, '<leader>ff', builtin.find_files, { desc = 'Files' })
 	vim.keymap.set({ 'n', 'v' }, '<leader>fg', builtin.git_files, { desc = 'Git files' })
-	vim.keymap.set({ 'n', 'v' }, '<leader>fo', builtin.buffers, { desc = 'Buffers' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fG', builtin.git_status, { desc = 'Git status' })
+
 	vim.keymap.set({ 'n', 'v' }, '<leader>fl', builtin.live_grep, { desc = 'Live grep' })
-	vim.keymap.set({ 'n', 'v' }, '<leader>fs', builtin.grep_string, { desc = 'Grep string' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fL', function()
+		local glob = vim.fn.input("Glob > ")
+		builtin.live_grep {
+			prompt_title = "Live grep (glob '" .. glob .. "')",
+			glob_pattern = glob
+		}
+	end, { desc = 'Live grep (specify glob filter)' })
+
+	vim.keymap.set({ 'n', 'v' }, '<leader>fs', builtin.grep_string, { desc = 'Grep string (current)' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fS', function()
+		builtin.grep_string {
+			search = vim.fn.input("Grep > ")
+		}
+	end, { desc = 'Grep string (specify)' })
+
+	vim.keymap.set({ 'n', 'v' }, '<leader>fo', builtin.buffers, { desc = 'Buffers' })
+	vim.keymap.set({ 'n', 'v' }, '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'Current buffer' })
 	vim.keymap.set({ 'n', 'v' }, '<leader>fh', function()
 		vim.cmd('Telescope harpoon marks')
 	end, { desc = '' })
 
 	vim.keymap.set({ 'n', 'v' }, '<leader>fq', builtin.quickfix, { desc = 'Quickfix' })
 	vim.keymap.set({ 'n', 'v' }, '<leader>ft', builtin.builtin, { desc = 'Telescopes' })
-	vim.keymap.set({ 'n', 'v' }, '<leader>fG', builtin.git_status, { desc = 'Git status' })
-	vim.keymap.set({ 'n', 'v' }, '<leader>fc', builtin.current_buffer_fuzzy_find, { desc = 'Current buffer' })
+
 	require("telescope").load_extension "file_browser"
 
 	-- open file_browser with the path of the current buffer
