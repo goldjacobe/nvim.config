5fa4ca48e269040f09f23c2a06f46429b2a950c4
Jacob Gold <jacob@graphite.dev>
2023-11-03 13:00:33 -0700

    feat: switch to catppuccin-frappe

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,10 +1,11 @@
 if not vim.g.vscode then
-	local bamboo = require('bamboo')
-	bamboo.setup {
-		-- Main options --
-		-- NOTE: to use the light theme, set `vim.o.background = 'light'`
-		style = 'multiplex',           -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
-		toggle_style_key = '<leader>ct', -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
-	}
-	bamboo.load()
+	-- local bamboo = require('bamboo')
+	-- bamboo.setup {
+	-- 	-- Main options --
+	-- 	-- NOTE: to use the light theme, set `vim.o.background = 'light'`
+	-- 	style = 'multiplex',           -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
+	-- 	toggle_style_key = '<leader>ct', -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
+	-- }
+	-- bamboo.load()
+	vim.cmd('colorscheme catppuccin-frappe')
 end
