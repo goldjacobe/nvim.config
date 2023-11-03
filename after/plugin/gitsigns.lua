65c50dd5954bd6dcba46eb42f029e0e0ffd3a56a
Jacob Gold <jacob@graphite.dev>
2023-11-02 18:22:54 -0700

    feat: gitsigns improvmenets

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,13 +1,6 @@
 if not vim.g.vscode then
 	require('gitsigns').setup({
 		-- See `:help gitsigns.txt`
-		signs = {
-			add = { text = '+' },
-			change = { text = '~' },
-			delete = { text = '_' },
-			topdelete = { text = '‾' },
-			changedelete = { text = '~' },
-		},
 		current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 		current_line_blame_opts = {
 			virt_text = true,
@@ -40,19 +33,19 @@ if not vim.g.vscode then
 
 			-- HunkActions
 			map('n', '<leader>ghs', gs.stage_hunk, { desc = 'Stage hunk' })
-			map('n', '<leader>ghr', gs.reset_hunk, { desc = 'Reset hunk' })
+			map('n', '<leader>ghS', gs.stage_buffer, { desc = 'Stage buffer' })
 			map('v', '<leader>ghs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
 				{ desc = 'Stage hunk' })
 			map('v', '<leader>ghr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
 				{ desc = 'Reset hunk' })
-			map('n', '<leader>ghS', gs.stage_buffer, { desc = 'Stage buffer' })
 			map('n', '<leader>ghu', gs.undo_stage_hunk, { desc = 'Undo stage hunk' })
+			map('n', '<leader>ghr', gs.reset_hunk, { desc = 'Reset hunk' })
 			map('n', '<leader>ghR', gs.reset_buffer, { desc = 'Reset buffer' })
 			map('n', '<leader>ghp', gs.preview_hunk, { desc = 'Preview hunk' })
 			map('n', '<leader>ghb', function() gs.blame_line { full = true } end, { desc = 'Show full blame for line' })
-			map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = 'Toggle blame for current line' })
 			map('n', '<leader>ghd', gs.diffthis, { desc = 'Diff hunk' })
 			map('n', '<leader>ghD', function() gs.diffthis('~') end, { desc = 'Diff hunk against previous commit' })
+			map('n', '<leader>gtb', gs.toggle_current_line_blame, { desc = 'Toggle blame for current line' })
 			map('n', '<leader>gtd', gs.toggle_deleted, { desc = 'Toggle deleted line view' })
 
 			-- Text object
