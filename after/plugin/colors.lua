d91866870299955242728833e7415c083bd36e98
Jacob Gold <jacob@graphite.dev>
2023-11-04 18:47:53 -0700

    feat: adjust color scheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,5 +7,36 @@ if not vim.g.vscode then
 	-- 	toggle_style_key = '<leader>ct', -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
 	-- }
 	-- bamboo.load()
-	vim.cmd('colorscheme catppuccin-frappe')
+
+	require('catppuccin').setup {
+		flavour = 'frappe',
+
+		background = {
+			light = 'latte',
+			dark = 'mocha',
+		},
+
+		dim_inactive = {
+			enabled = true, -- dims the background color of inactive window
+			shade = "dark",
+			percentage = 0.15, -- percentage of the shade to apply to the inactive window
+		},
+
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			harpoon = true,
+			mason = true,
+			noice = true,
+			notify = true,
+			nvimtree = true,
+			treesitter = true,
+			treesitter_context = true,
+			telescope = { enabled = true },
+			lsp_trouble = true,
+			which_key = true,
+		}
+	}
+
+	vim.cmd.colorscheme 'catppuccin'
 end
