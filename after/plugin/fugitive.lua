b5dce292614fbf217771f8e38ac865c4f05c6bba
Jacob Gold <jacob@graphite.dev>
2023-11-07 15:10:33 -0800

    feat: status map

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,7 +1,31 @@
 -- open or focus fugitive
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Fugitive staging area' });
-vim.keymap.set('n', '<leader>gb', function() vim.cmd('Git blame') end, { desc = 'Fugitive blame' });
-vim.keymap.set('n', '<leader>gH', function() vim.cmd('GBrowse') end, { desc = 'View on GitHub' });
+vim.keymap.set(
+	'n',
+	'<leader>gs',
+	vim.cmd.Git,
+	{ desc = 'Fugitive staging area' }
+)
+
+vim.keymap.set(
+	'n',
+	'<leader>gS',
+	function() vim.cmd('Git status') end,
+	{ desc = 'Fugitive staging area' }
+)
+
+vim.keymap.set(
+	'n',
+	'<leader>gb',
+	function() vim.cmd('Git blame') end,
+	{ desc = 'Fugitive blame' }
+)
+
+vim.keymap.set(
+	'n',
+	'<leader>gH',
+	function() vim.cmd('GBrowse') end,
+	{ desc = 'View on GitHub' }
+)
 
 -- close fugitive
 vim.keymap.set('n', '<leader>gq', function()
