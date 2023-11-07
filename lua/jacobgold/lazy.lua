a8c9206dae65974875f95a46f550c6afb264ca64
Jacob Gold <jacob@graphite.dev>
2023-11-07 15:02:35 -0800

    feat: origami

--- lua/jacobgold/lazy.lua
+++ lua/jacobgold/lazy.lua
@@ -13,6 +13,11 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+		"chrisgrieser/nvim-origami",
+		event = "BufReadPost", -- later or on keypress would prevent saving folds
+		opts = true,        -- needed even when using default config
+	},
+	{
 		'goldjacobe/globalcolor',
 		cond = not vim.g.vscode
 	},
