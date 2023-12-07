622da1af3e1ed4ac220e594bdd85913d9491a59f
Jacob Gold <jacob@graphite.dev>
2023-12-06 18:33:41 -0800

    chore: migrate to harpoon2

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,18 +1,51 @@
 if not vim.g.vscode then
-	local mark = require('harpoon.mark')
-	local ui = require('harpoon.ui')
+	local harpoon = require("harpoon")
 
-	vim.keymap.set({ "n", "v" }, "<leader>a", mark.add_file, { desc = 'Harpoon add mark' })
-	vim.keymap.set({ "n", "v" }, "<leader>h", ui.toggle_quick_menu, { desc = 'Harpoon list' })
+	vim.keymap.set("n", "<leader>a", function()
+		harpoon:list():append()
+	end, { desc = 'Harpoon add mark' })
 
-	vim.keymap.set({ "n", "v" }, "<leader>1", function() ui.nav_file(1) end, { desc = 'Harpoon mark 1' })
-	vim.keymap.set({ "n", "v" }, "<leader>2", function() ui.nav_file(2) end, { desc = 'Harpoon mark 2' })
-	vim.keymap.set({ "n", "v" }, "<leader>3", function() ui.nav_file(3) end, { desc = 'Harpoon mark 3' })
-	vim.keymap.set({ "n", "v" }, "<leader>4", function() ui.nav_file(4) end, { desc = 'Harpoon mark 4' })
-	vim.keymap.set({ "n", "v" }, "<leader>5", function() ui.nav_file(5) end, { desc = 'Harpoon mark 5' })
-	vim.keymap.set({ "n", "v" }, "<leader>6", function() ui.nav_file(6) end, { desc = 'Harpoon mark 6' })
-	vim.keymap.set({ "n", "v" }, "<leader>7", function() ui.nav_file(7) end, { desc = 'Harpoon mark 7' })
-	vim.keymap.set({ "n", "v" }, "<leader>8", function() ui.nav_file(8) end, { desc = 'Harpoon mark 8' })
-	vim.keymap.set({ "n", "v" }, "<leader>9", function() ui.nav_file(9) end, { desc = 'Harpoon mark 9' })
-	vim.keymap.set({ "n", "v" }, "<leader>0", function() ui.nav_file(10) end, { desc = 'Harpoon mark 10' })
+	vim.keymap.set("n", "<leader>h", function()
+		harpoon.ui:toggle_quick_menu(harpoon:list())
+	end, { desc = 'Harpoon list' })
+
+	vim.keymap.set("n", "<leader>1", function()
+		harpoon:list():select(1)
+	end, { desc = 'Harpoon mark 1' })
+
+	vim.keymap.set("n", "<leader>2", function()
+		harpoon:list():select(2)
+	end, { desc = 'Harpoon mark 2' })
+
+	vim.keymap.set("n", "<leader>3", function()
+		harpoon:list():select(3)
+	end, { desc = 'Harpoon mark 3' })
+
+	vim.keymap.set("n", "<leader>4", function()
+		harpoon:list():select(4)
+	end, { desc = 'Harpoon mark 4' })
+
+	vim.keymap.set("n", "<leader>5", function()
+		harpoon:list():select(5)
+	end, { desc = 'Harpoon mark 5' })
+
+	vim.keymap.set("n", "<leader>6", function()
+		harpoon:list():select(6)
+	end, { desc = 'Harpoon mark 6' })
+
+	vim.keymap.set("n", "<leader>7", function()
+		harpoon:list():select(7)
+	end, { desc = 'Harpoon mark 7' })
+
+	vim.keymap.set("n", "<leader>8", function()
+		harpoon:list():select(8)
+	end, { desc = 'Harpoon mark 8' })
+
+	vim.keymap.set("n", "<leader>9", function()
+		harpoon:list():select(9)
+	end, { desc = 'Harpoon mark 9' })
+
+	vim.keymap.set("n", "<leader>0", function()
+		harpoon:list():select(10)
+	end, { desc = 'Harpoon mark 10' })
 end
--- lua/jacobgold/lazy.lua
+++ lua/jacobgold/lazy.lua
@@ -149,6 +149,8 @@ require("lazy").setup({
 	},
 	{
 		'ThePrimeagen/harpoon',
+		branch = 'harpoon2',
+		dependencies = { 'nvim-lua/plenary.nvim' },
 		cond = not vim.g.vscode
 	},
 	{
