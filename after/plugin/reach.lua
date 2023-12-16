d89916d445f7cbed8d0be02240370c5269f040c8
Jacob Gold <jacob@graphite.dev>
2023-12-15 18:27:50 -0800

    feat: recency sort for reach list

--- after/plugin/reach.lua
+++ after/plugin/reach.lua
@@ -12,11 +12,28 @@ if not vim.g.vscode then
 		}
 	end, { desc = 'Reach buffers' })
 
+	local last_active_times = {}
+	local track_last_active_time = vim.api.nvim_create_augroup('track_last_active_time_for_reach', {})
+	vim.api.nvim_create_autocmd('BufEnter', {
+		group = track_last_active_time,
+		callback = function()
+			last_active_times[vim.api.nvim_get_current_buf()] = os.time()
+		end
+	})
+
 	vim.keymap.set("n", "<leader>B", function()
 		reach.buffers {
 			handle = 'dynamic',
 			show_current = true,
-			sort = function(a, b) return a < b end,
+			sort = function(a, b)
+				local atime = last_active_times[a]
+				local btime = last_active_times[b]
+				print(a, b, atime, btime)
+				if not atime and not btime then return a < b end
+				if not atime then return false end
+				if not btime then return true end
+				return atime > btime
+			end,
 		}
 	end, { desc = 'Reach buffers' })
 
