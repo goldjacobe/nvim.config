e707b21b478f9f90f4c29403afa9a768e0041601
Jacob Gold <jacob@graphite.dev>
2024-10-04 18:14:05 -0400

    feat: close unsupported windows

--- after/plugin/auto-session.lua
+++ after/plugin/auto-session.lua
@@ -1,10 +1,10 @@
 if not vim.g.vscode then
 	require("auto-session").setup {
-		log_level = "error",
+		close_unsupported_windows = false,
 
 		cwd_change_handling = {
 			post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-				require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+				require("lualine").refresh()  -- refresh lualine so the new session name is displayed in the status bar
 			end,
 		},
 	}
