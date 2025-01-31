8bdc4b5ff5df5ff6f1fd5fde40028abd1248649e
Jacob Gold <jacob@graphite.dev>
2025-01-30 17:40:50 -0800

    feat: i didnt need to do that myself

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,19 +1,4 @@
 return {
-  -- disable neo-tree
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
-    keys = {
-      {
-        "<leader>e",
-        false,
-      },
-      {
-        "<leader>E",
-        false,
-      },
-    },
-  },
   -- disable flash
   { "folke/flash.nvim", enabled = false },
 }
--- lua/plugins/explore.lua
+++ lua/plugins/explore.lua
@@ -18,7 +18,6 @@ return {
     },
   },
   setup = function()
-    -- Source: https://github.com/stevearc/oil.nvim/blob/add50252b5e9147c0a09d36480d418c7e2737472/lua/oil/init.lua#L1177C1-L1184C6
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
     -- If netrw was already loaded, clear this augroup
